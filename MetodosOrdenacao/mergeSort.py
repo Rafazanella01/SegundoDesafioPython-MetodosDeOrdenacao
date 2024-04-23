d391ad01031a6ae0f5416c422222891a184cd34a
Leonardo Reuter <leo.mreuter@gmail.com>
2024-04-22 21:19:55 -0300

    Show de bola

---
+++ MetodosOrdenacao/mergeSort.py
@@ -0,0 +1,27 @@
+class mergeSort:
+    def mergesort(lista, inicio=0, fim=None):
+        if fim is None:
+            fim = len(lista)
+        if(fim - inicio > 1):
+            meio = (fim + inicio)//2
+            mergesort(lista, inicio, meio)
+            mergesort(lista, meio, fim)
+            merge(lista, inicio, meio, fim)
+
+    def merge(lista, inicio, meio, fim):
+        esquerda = lista[inicio:meio]
+        direita = lista[meio:fim]
+        e, d = 0, 0
+        for k in range(inicio, fim):
+            if e >= len(esquerda):
+                lista[k] = direita[d]
+                d = d + 1
+            elif d >= len(direita):
+                lista[k] = esquerda[e]
+                e = e + 1
+            elif esquerda[e] < direita[d]:
+                lista[k] = esquerda[e]
+                e = e + 1
+            else:
+                lista[k] = direita[d]
+                d = d + 1
